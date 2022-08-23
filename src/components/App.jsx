db3717e2ad463a40736f33b8a672c9122a9d5d32
Akmal Azami <akmalm.makhkamov@gmail.com>
2022-08-23 08:09:31 +0100

    User profile with watchlist added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { CssBaseline } from "@mui/material";
 import { Route, Switch } from "react-router-dom";
 import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 import useStyles from "./styles";
+import useAlan from "./Alan";
 
 const App = () => {
 	const classes = useStyles();
-
+	const alanBtnContainer = useRef();
+	useAlan();
 	return (
-		<div className="classes.root">
+		<div className={classes.root}>
 			<CssBaseline />
 			<NavBar />
-			<main className="classes.content">
-				<div className="classes.toolbar" />
+
+			<main className={classes.content}>
+				<div className={classes.toolbar} />
 				<Switch>
-					<Route exact path={["/", "/movies"]}>
+					<Route exact path={["/", "/approved"]}>
 						<Movies />
 					</Route>
-					<Route exact path="/movies/:id">
+					<Route exact path="/movie/:id">
 						<MovieInformation />
 					</Route>
 					<Route exact path="/actors/:id">
@@ -28,6 +31,7 @@ const App = () => {
 					</Route>
 				</Switch>
 			</main>
+			<div ref={alanBtnContainer} />
 		</div>
 	);
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Typography, Button, Box } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
+import { userSelector } from "../../features/auth";
+import { useGetListQuery } from "../../services/TMDB";
+import RatedCards from "./../RatedCards/RatedCards";
 
 const Profile = () => {
-	console.log("Profile");
-	return <div>Profile</div>;
+	const { user } = useSelector(userSelector);
+	const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({
+		listName: "favorite/movies",
+		accountId: user.id,
+		sessionId: localStorage.getItem("session_id"),
+		page: 1,
+	});
+	const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery(
+		{
+			listName: "watchlist/movies",
+			accountId: user.id,
+			sessionId: localStorage.getItem("session_id"),
+			page: 1,
+		}
+	);
+
+	useEffect(() => {
+		refetchFavorites();
+		refetchWatchlisted();
+	}, []);
+
+	console.log(user, "Profile - user");
+
+	const logout = () => {
+		localStorage.clear();
+		window.location.href = "/";
+	};
+
+	return (
+		<Box>
+			<Box display="flex" justifyContent="space-between">
+				<Typography variant="h4" gutterBottom>
+					{" "}
+					My profile
+				</Typography>
+				<Button color="inherit" onClick={logout}>
+					Logout &nbsp; <ExitToApp />
+				</Button>
+			</Box>
+			{!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
+				<Typography variant="h5">
+					Add favourite or watchlist some movies to see the here!
+				</Typography>
+			) : (
+				<Box>
+					{" "}
+					<RatedCards title="Favourite Movies" data={favoriteMovies} />
+					<RatedCards title="Watchlist" data={watchlistMovies} />
+				</Box>
+			)}
+		</Box>
+	);
 };
 
 export default Profile;
