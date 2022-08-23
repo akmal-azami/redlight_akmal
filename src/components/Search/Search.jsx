b250d6760581ca803fd96e2331cda77b45554345
Akmal Azami <akmalm.makhkamov@gmail.com>
2022-08-23 08:04:11 +0100

    Sidebar created, and search component added to Navbar

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,46 @@
+import React, { useState  } from "react";
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+
+import useStyles from "./styles";
+import { searchMovie } from "../../features/currentGenreOrCategory";
+
+const Search = () => {
+	const classes = useStyles();
+	const [query, setQuery] = useState("");
+	const dispatch = useDispatch();
+	const location = useLocation();
+
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+			console.log("Search query", query);
+			dispatch(searchMovie(query));
+		}
+	};
+
+	const handleClick = () => {
+		return dispatch(searchMovie(query));
+	};
+
+	if (location.pathname !== "/") return null;
+
+	return (
+		<div className={classes.searchContainer}>
+			<TextField
+				onKeyPress={handleKeyPress}
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				variant="standard"
+				InputProps={{
+					className: classes.input,
+					startAdornent: <InputAdornment position="start"></InputAdornment>,
+				}}
+			/>
+			<SearchIcon onClick={handleClick} style={{ cursor: "pointer" }} />
+		</div>
+	);
+};
+
+export default Search;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,23 +12,21 @@ import {
 import { Link } from "react-router-dom";
 import { useTheme } from "@mui/styles";
 import { useDispatch, useSelector } from "react-redux";
+import redlightLogo from "./redlightLogo.png";
+import redlightLogoDark from "./redlightLogoDark.png";
 
-import { useGetGenresQuery } from "../../services/TMDB";
 import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
+import { useGetGenresQuery } from "../../services/TMDB";
 import useStyles from "./styles";
 import genreIcons from "../../assets/genres";
-import { NearMeDisabledRounded } from "@mui/icons-material";
 
 const categories = [
 	{ label: "Popular", value: "popular" },
 	{ label: "Top Rated", value: "top_rated" },
 	{ label: "Upcoming", value: "upcoming" },
 ];
-
-const redLogo =
-	"https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png";
-const blueLogo =
-	"https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
+const redLogo = <img src={redlightLogo} alt="/" />;
+const blueLogo = <img src={redlightLogoDark} alt="/" />;
 
 const Sidebar = ({ setMobileOpen }) => {
 	const { genreIdOrCategoryName } = useSelector(
@@ -38,6 +36,12 @@ const Sidebar = ({ setMobileOpen }) => {
 	const classes = useStyles();
 	const { data, isFetching } = useGetGenresQuery();
 	const dispatch = useDispatch();
+
+	console.log(data, "Genres - Sidebar");
+
+	useEffect(() => {
+		setMobileOpen(false);
+	}, [genreIdOrCategoryName]);
 	// useEffect(() => {
 	// 	setMobileOpen(false);
 	// }, [genreIdOrCategoryName]);
@@ -48,7 +52,7 @@ const Sidebar = ({ setMobileOpen }) => {
 				<img
 					className={classes.image}
 					src={theme.palette.mode === "light" ? redLogo : blueLogo}
-					alt="FILMPIRE LOGO"
+					alt="Redlight LOGO"
 				/>
 			</Link>
 			<Divider />
@@ -76,7 +80,7 @@ const Sidebar = ({ setMobileOpen }) => {
 				<ListSubheader> Genres </ListSubheader>
 				{isFetching ? (
 					<Box display="flex" justifyContent="center">
-						<CircularProgress size="4rem" />
+						<CircularProgress />
 					</Box>
 				) : (
 					data.genres.map(({ name, id }) => (
